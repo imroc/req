4112e5c0ff10452583a9101bf0264d5690626b97
roc <roc@imroc.cc>
2022-01-21 11:44:23 +0800

    port go1.18's strings.Cut to cutString to support lower go version

--- common.go
+++ common.go
@@ -1,5 +1,7 @@
 package req
 
+import "strings"
+
 const (
 	CONTENT_TYPE_APPLICATION_JSON_UTF8 = "application/json; charset=UTF-8"
 	CONTENT_TYPE_APPLICATION_XML_UTF8  = "application/xml; charset=UTF-8"
@@ -7,3 +9,17 @@ const (
 	CONTENT_TYPE_TEXT_HTML_UTF8        = "text/html; charset=UTF-8"
 	CONTENT_TYPE_TEXT_PLAIN_UTF8       = "text/plain; charset=UTF-8"
 )
+
+
+// cutString is a string util function which is copied
+// from go1.18 strings package.
+// cutString slices s around the first instance of sep,
+// returning the text before and after sep.
+// The found result reports whether sep appears in s.
+// If sep does not appear in s, cut returns s, "", false.
+func cutString(s, sep string) (before, after string, found bool) {
+	if i := strings.Index(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, "", false
+}
