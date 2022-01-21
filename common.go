e035f7bf7c51266eec8f15145c0cac28915f7f6c
roc <roc@imroc.cc>
2022-01-21 11:53:51 +0800

    fix another bytes.Cut

--- common.go
+++ common.go
@@ -1,6 +1,9 @@
 package req
 
-import "strings"
+import (
+	"bytes"
+	"strings"
+)
 
 const (
 	CONTENT_TYPE_APPLICATION_JSON_UTF8 = "application/json; charset=UTF-8"
@@ -22,4 +25,17 @@ func cutString(s, sep string) (before, after string, found bool) {
 		return s[:i], s[i+len(sep):], true
 	}
 	return s, "", false
+}
+
+// bytesCut slices s around the first instance of sep,
+// returning the text before and after sep.
+// The found result reports whether sep appears in s.
+// If sep does not appear in s, cut returns s, nil, false.
+//
+// Cut returns slices of the original slice s, not copies.
+func bytesCut(s, sep []byte) (before, after []byte, found bool) {
+	if i := bytes.Index(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, nil, false
 }
--- textproto_reader.go
+++ textproto_reader.go
@@ -564,7 +564,7 @@ func (r *textprotoReader) ReadMIMEHeader() (textproto.MIMEHeader, error) {
 		}
 
 		// Key ends at first colon.
-		k, v, ok := bytes.Cut(kv, colon)
+		k, v, ok := bytesCut(kv, colon)
 		if !ok {
 			return m, protocolError("malformed MIME header line: " + string(kv))
 		}
