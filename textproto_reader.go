f189f9ef44289f2737c579ac427854c6cd4b912e
roc <roc@imroc.cc>
2022-01-28 21:12:01 +0800

    remove unused func in textproto_reader.go

--- textproto_reader.go
+++ textproto_reader.go
@@ -26,10 +26,6 @@ func (e *codeError) Error() string {
 	return fmt.Sprintf("%03d %s", e.Code, e.Msg)
 }
 
-func isASCIISpace(b byte) bool {
-	return b == ' ' || b == '\t' || b == '\n' || b == '\r'
-}
-
 func isASCIILetter(b byte) bool {
 	b |= 0x20 // make lower case
 	return 'a' <= b && b <= 'z'
