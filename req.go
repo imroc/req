9784e32795eac84186c754ea187ac136f55419ab
roc <roc@imroc.cc>
2022-02-05 01:06:16 +0800

    update default UserAgent to v3

--- req.go
+++ req.go
@@ -2,7 +2,7 @@ package req
 
 const (
 	hdrUserAgentKey   = "User-Agent"
-	hdrUserAgentValue = "req/v2 (https://github.com/imroc/req)"
+	hdrUserAgentValue = "req/v3 (https://github.com/imroc/req)"
 	hdrContentTypeKey = "Content-Type"
 	plainTextType     = "text/plain; charset=utf-8"
 	jsonContentType   = "application/json; charset=utf-8"
