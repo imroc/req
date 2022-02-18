ac6cf57a148024d85d0389cd6de580e77151d544
roc <roc@imroc.cc>
2022-02-18 16:22:48 +0800

    add TestPeekDrain

---
+++ decode_test.go
@@ -0,0 +1,14 @@
+package req
+
+import "testing"
+
+func TestPeekDrain(t *testing.T) {
+	a := autoDecodeReadCloser{peek: []byte("test")}
+	p := make([]byte, 2)
+	n, _ := a.peekDrain(p)
+	assertEqual(t, 2, n)
+	assertEqual(t, true, a.peek != nil)
+	n, _ = a.peekDrain(p)
+	assertEqual(t, 2, n)
+	assertEqual(t, true, a.peek == nil)
+}
