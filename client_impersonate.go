1913cbbaf54bb8d1a0bf1a7665aeb7f78f5eb5dc
roc <roc@imroc.cc>
2023-08-04 15:00:37 +0800

    add ImpersonateFirefox

--- client_impersonate.go
+++ client_impersonate.go
@@ -29,6 +29,13 @@ var (
 		},
 	}
 
+	chromePseudoHeaderOrder = []string{
+		":method",
+		":authority",
+		":scheme",
+		":path",
+	}
+
 	chromeHeaderOrder = []string{
 		"host",
 		"pragma",
@@ -47,13 +54,6 @@ var (
 		"accept-encoding",
 		"accept-language",
 		"cookie",
-	}
-
-	chromePseudoHeaderOrder = []string{
-		":method",
-		":authority",
-		":scheme",
-		":path",
 	}
 
 	chromeHeaders = map[string]string{
@@ -80,12 +80,130 @@ var (
 
 // ImpersonateChrome impersonates Chrome browser (version 109).
 func (c *Client) ImpersonateChrome() *Client {
-	c.SetTLSFingerprint(utls.HelloChrome_106_Shuffle).
-		SetCommonPseudoHeaderOder(chromePseudoHeaderOrder...).
-		SetCommonHeaders(chromeHeaders).
-		SetCommonHeaderOrder(chromeHeaderOrder...).
+	c.
+		SetTLSFingerprint(utls.HelloChrome_106_Shuffle). // Chrome 106~109 shares the same tls fingerprint.
 		SetHTTP2SettingsFrame(chromeHttp2Settings...).
 		SetHTTP2ConnectionFlow(15663105).
+		SetCommonPseudoHeaderOder(chromePseudoHeaderOrder...).
+		SetCommonHeaderOrder(chromeHeaderOrder...).
+		SetCommonHeaders(chromeHeaders).
 		SetHTTP2HeaderPriority(chromeHeaderPriority)
+	return c
+}
+
+var (
+	firefoxHttp2Settings = []http2.Setting{
+		{
+			ID:  http2.SettingHeaderTableSize,
+			Val: 65536,
+		},
+		{
+			ID:  http2.SettingInitialWindowSize,
+			Val: 131072,
+		},
+		{
+			ID:  http2.SettingMaxFrameSize,
+			Val: 16384,
+		},
+	}
+	firefoxPriorityFrames = []http2.PriorityFrame{
+		{
+			StreamID: 3,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: false,
+				Weight:    200,
+			},
+		},
+		{
+			StreamID: 5,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: false,
+				Weight:    100,
+			},
+		},
+		{
+			StreamID: 7,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: false,
+				Weight:    0,
+			},
+		},
+		{
+			StreamID: 9,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 7,
+				Exclusive: false,
+				Weight:    0,
+			},
+		},
+		{
+			StreamID: 11,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 3,
+				Exclusive: false,
+				Weight:    0,
+			},
+		},
+		{
+			StreamID: 13,
+			PriorityParam: http2.PriorityParam{
+				StreamDep: 0,
+				Exclusive: false,
+				Weight:    240,
+			},
+		},
+	}
+	firefoxPseudoHeaderOrder = []string{
+		":method",
+		":path",
+		":authority",
+		":scheme",
+	}
+	firefoxHeaderOrder = []string{
+		"user-agent",
+		"accept",
+		"accept-language",
+		"accept-encoding",
+		"referer",
+		"cookie",
+		"upgrade-insecure-requests",
+		"sec-fetch-dest",
+		"sec-fetch-mode",
+		"sec-fetch-site",
+		"sec-fetch-user",
+		"te",
+	}
+	firefoxHeaders = map[string]string{
+		"user-agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0",
+		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+		"accept-language":           "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+		"upgrade-insecure-requests": "1",
+		"sec-fetch-dest":            "document",
+		"sec-fetch-mode":            "navigate",
+		"sec-fetch-site":            "same-origin",
+		"sec-fetch-user":            "?1",
+		//"te":                        "trailers",
+	}
+	firefoxHeaderPriority = http2.PriorityParam{
+		StreamDep: 13,
+		Exclusive: false,
+		Weight:    41,
+	}
+)
+
+// ImpersonateFirefox impersonates Firefox browser (version 105).
+func (c *Client) ImpersonateFirefox() *Client {
+	c.
+		SetTLSFingerprint(utls.HelloFirefox_105).
+		SetHTTP2SettingsFrame(firefoxHttp2Settings...).
+		SetHTTP2ConnectionFlow(12517377).
+		SetHTTP2PriorityFrames(firefoxPriorityFrames...).
+		SetCommonPseudoHeaderOder(firefoxPseudoHeaderOrder...).
+		SetCommonHeaderOrder(firefoxHeaderOrder...).
+		SetCommonHeaders(firefoxHeaders).
+		SetHTTP2HeaderPriority(firefoxHeaderPriority)
 	return c
 }
