b3d0d9e6bc5177eda4ef91c6978763217ac3b86a
fan-tastic-z <fantastic.fun.zf@gmail.com>
2025-11-10 10:17:30 +0800

    fix: chrome headers accept add application/json

--- client_impersonate.go
+++ client_impersonate.go
@@ -110,7 +110,7 @@ var (
 		"sec-ch-ua-platform":        `"macOS"`,
 		"upgrade-insecure-requests": "1",
 		"user-agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"accept":                    "text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 		"sec-fetch-site":            "none",
 		"sec-fetch-mode":            "navigate",
 		"sec-fetch-user":            "?1",
